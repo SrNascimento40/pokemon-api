0bd82560dabb5a7b870d4e9b534806a0da82a42a
Wallace Nascimento <wallacediogo10@hotmail.com>
2022-11-10 18:44:22 -0300

    feature: fetch api | style | initial page

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+
+// const offset = 0
+// const limit = 10
+// const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+//fetch requisição get
+// fetch(url)
+//     .then(function (response) {
+//         //vai retornar uma promisse o then fala "quando ela chegar faça isso"
+//         console.log(response);
+//         response.json()
+//             .then(function(responseBody) {
+//                 //vai retornar convertido, sem ser json
+//                 console.log(responseBody);
+//             })
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     })
+//     .finally(function () {
+//         console.log('requisição concluída');
+//     })
+
+//acima versão de estudo
+
+//abaixo versão correta de como deve ser
+
+function convertPokemonToHtml(pokemon) {
+    return `
+    <li class="pokemon ${pokemon.type}">
+        <span class="number">${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detailed">
+            <ol>
+                ${pokemon.types.map((type) => `<li class="type ${pokemon.type}">${type}</li>`).join('')}
+            </ol>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.number}.png"
+                class="pokeImg" alt="${pokemon.name}"/>
+        </div>
+    </li>
+    `
+}
+
+const pokemonOl = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemonList = []) => {
+
+    //tudo abaixo em uma linha
+    const newHtml = pokemonList.map(convertPokemonToHtml).join('')
+    pokemonOl.innerHTML = newHtml
+    //forma passo a passo
+
+    // //vai receber cada item da pokemonList transformar em html e depois adicionar tudo na lista
+    // const newList = pokemonList.map(convertPokemonToHtml)
+
+    // //vai pegar todas as listas e transformar em um html
+    // const newHtml = newList.join('')
+
+    // //vai pegar o html gerado e adicionar no documento html
+    // pokemonOl.innerHTML += newHtml
+})
---
+++ assets/js/poke-api.js
@@ -0,0 +1,32 @@
+const pokeApi = {}
+
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+    
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types //vai pegar a primeira posição do array
+
+    pokemon.types = types
+    pokemon.type = type
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetails = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 20) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    return fetch(url)
+        .then((response) => response.json()) //quando a promise resolver vai pro proximo then
+        .then((jsonBody) => jsonBody.results) //pega só os resultados, excluindo detalhes irrelevantes
+        .then((pokemons) => pokemons.map((pokeApi.getPokemonDetails))) //mapeada lista com sites de requisições para cada pokemon
+        .then((detailedRequests) => Promise.all(detailedRequests)) //vai fazer fetch da requisição de cada pokemon
+        .then((pokemonDetails) =>pokemonDetails) //vai retornar a lista com o resultado de cada requisição particular de pokemon, com mais detalhes
+        .catch((error) => console.error(error)) //se der erro vai aparecer o que foi
+} 
